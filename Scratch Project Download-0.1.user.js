a079a132fe3a07f47e68dabd2231f54859ad0dcd
netmin012 <115701341+netmin012@users.noreply.github.com>
2023-12-08 08:22:06 +0900

    Update Scratch Project Download-0.1.user.js
    ダウンロードボタンの位置の変更
    ページの一番上に追加されるようにした

--- Scratch Project Download-0.1.user.js
+++ Scratch Project Download-0.1.user.js
@@ -1,36 +1,35 @@
 // ==UserScript==
-// @name        Scratch Project Download
-// @namespace   http://tampermonkey.net/
-// @version     0.1
+// @name       Scratch Project Download
+// @namespace  http://tampermonkey.net/
+// @version    0.1
 // @description Add a download button to Scratch projects
-// @author      You
-// @match       https://scratch.mit.edu/projects/*
-// @icon　　　　https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://scratch.mit.edu&size=180
-// @grant       none
+// @author     You
+// @match      https://scratch.mit.edu/projects/*
+// @icon       https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://scratch.mit.edu&size=180
+// @grant      none
 // ==/UserScript==
 
 // ページが完全にロードされた後にスクリプトを実行
 window.addEventListener('load', function() {
-  // プロジェクトIDを取得
-  var projectId = window.location.pathname.split('/')[2];
+ // プロジェクトIDを取得
+ var projectId = window.location.pathname.split('/')[2];
 
-  // ダウンロードボタンを作成
-  var downloadButton = document.createElement('button');
-  downloadButton.textContent = 'Download Project';
-  downloadButton.onclick = function() {
-      // URLを作成
-      var url = 'https://forkphorus.github.io/sb-downloader/?id=' + projectId;
+ // ダウンロードボタンを作成
+ var downloadButton = document.createElement('button');
+ downloadButton.textContent = 'プロジェクトをダウンロード';
+ downloadButton.onclick = function() {
+     // URLを作成
+     var url = 'https://forkphorus.github.io/sb-downloader/?id=' + projectId;
 
-      // 新しいポップアップウィンドウを開く
-      var popupWindow = window.open(url, '_blank', 'height=600,width=800');
+     // 新しいポップアップウィンドウを開く
+     var popupWindow = window.open(url, '_blank', 'height=600,width=800');
 
-      // ポップアップウィンドウがロードされたときにダウンロードが完了したと判断し、ウィンドウを閉じる
-      popupWindow.onload = function() {
-          popupWindow.close();
-      };
-  };
+     // ポップアップウィンドウがロードされたときにダウンロードが完了したと判断し、ウィンドウを閉じる
+     popupWindow.onload = function() {
+         popupWindow.close();
+     };
+ };
 
-  // ダウンロードボタンをページに追加
-  document.body.appendChild(downloadButton);
+ // ダウンロードボタンをページの最初に追加
+ document.body.insertBefore(downloadButton, document.body.firstChild);
 });
-
